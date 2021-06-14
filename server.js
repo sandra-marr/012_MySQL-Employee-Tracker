6dff507874205f599a8c78dff11c6c4d14a1a0c7
SPeats <srpeters44@gmail.com>
2021-06-13 21:02:01 -0700

    added initial inquirer prompt for the menu and switch statements to run each associated action

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
-
 const connection = mysql.createConnection({
   host: 'localhost',
 
@@ -18,5 +17,110 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) throw err;
-    //function to start app should go here
+    
+    menu();
 });
+
+const menu = () => {
+  inquirer
+    .prompt ([
+      {
+        type: "list",
+        name: "menu_choices",
+        message: "Please select an option from the list to get started.",
+        choices: 
+          [
+            "View all employees", 
+            "View all employees by Manager", 
+            "View all employees by Department", 
+            "Add employee", 
+            "Remove employee", 
+            "Update employee role", 
+            "Update employee manager",
+            "Exit"
+          ]
+      }
+    ])
+    .then ((answer) => {
+      switch (answer.menu_choices) {
+        case "View all employees":
+          viewAllEmployees()
+          break;
+        case "View all employees by Manager":
+          viewByManager()
+          break;
+        case "View all employees by Department":
+          viewByDepartment()
+          break;
+        case "Add employee":
+          addEmployee()
+          break;
+        case "Remove employee":
+          removeEmployee()
+          break;
+        case "Update employee role":
+          updateRole()
+          break;
+        case "Update employee manager":
+          updateManager()
+          break;
+        case "Exit":
+          connection.end();
+      } 
+    })
+}
+
+const viewAllEmployees = () => {
+
+  console.log("This is where you will view all employees");
+  
+  menu();
+
+};
+
+const viewByManager = () => {
+ 
+  console.log("This is where you will view all employees sorted by manager");
+
+  menu();
+};
+
+const viewByDepartment = () => {
+  
+  console.log("This is where you will view all employees sorted by department");
+
+  menu();
+
+};
+
+const addEmployee = () => {
+
+  console.log("This is where you will add an employee");
+  
+  menu();
+
+};
+
+const removeEmployee = () => {
+
+  console.log("This is where you will remove an employee");
+
+  menu();
+
+};
+
+const updateRole = () => {
+
+  console.log("This is where you will update an employee's role");
+
+  menu();
+
+};
+
+const updateManager = () => {
+
+  console.log("This is where you will update an employee's manager");
+
+  menu();
+  
+};
